45d53825000762b808fdb7975f58f7407de2d7fc
James Lee <jameslee.atx@gmail.com>
2016-05-19 15:57:51 -0500

    client src/view files update

---
+++ client/src/restDetails.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('Details')
+
+  .controller('DetailsCtrl', ['$scope', function($scope) {
+    
+
+   // display selected restaurant information
+  $scope.displayDetails = function (  ) {
+
+  }
+
+}]);
