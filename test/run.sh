e742ae55557d046595c8ea76ac8032f87e78e7e7
Zak <legobug2@hotmail.com>
2016-05-19 08:46:06 -0500

    Attempting to utilized code coverage tools 2

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,10 @@
 # FORKED_COVERAGE=1 ./node_modules/.bin/mocha --recursive -r test/test-helper.js "$@" -R mocha-lcov-reporter |\
 #  ./node_modules/coveralls/bin/coveralls.js
 
-./node_modules/.bin/mocha --recursive -r test/test-helper.js "$@"
+#./node_modules/.bin/mocha --recursive -r test/test-helper.js "$@"
 
-istanbul cover ./node_modules/mocha/bin/mocha --recursive -r test/test-helper.js "$@" --report lcovonly -- -R spec && cat ./coverage/lcov.info\
- | ./node_modules/coveralls/bin/coveralls.js
+istanbul cover ./node_modules/mocha/bin/_mocha --recursive -r test/test-helper.js test/**/*.js --report lcovonly -- -R spec
+cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+
+#istanbul cover ./node_modules/mocha/bin/mocha --recursive -r test/test-helper.js "$@" --report lcovonly -- -R spec && cat ./coverage/lcov.info\
+# | ./node_modules/coveralls/bin/coveralls.js
