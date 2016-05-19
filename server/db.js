e8c765f60b3e5645d5a2a8b665c39fc053f2673a
MKS-students <mks.students@hackreactor.com>
2016-05-19 16:35:13 -0500

    created database

--- server/db.js
+++ server/db.js
@@ -1,29 +1,56 @@
-exports = require('knex')({
+var knex = require('knex')({
   client: 'pg',
   connection: process.env.PG_CONNECTION_STRING,
   searchPath: 'knex,public'
-})
-
-exports.schema.createTable('users', function(table) {
-  table.increments();
-  table.string('name');
-  table.string('fb_id');
 });
 
-exports.schema.createTable('restaurants', function(table) {
-  table.increments();
-  table.string('yelp_id');
-  table.string('name');
-  table.string('address');
-  table.string('phone');
-  table.string('pric'e);
-  table.string('address');
-  table.string('photo_url');
-});
 
-exports.schema.createTable('user_restaurants', function(table) {
-  table.increments();
-  table.integer('user_id');
-  table.string('yelp_id');
-});
+module.exports = knex;
+
+
+knex.ensureSchema = function () {
+  return Promise.all([
+    knex.schema.hasTable('users').then(function (exists) {
+      if(!exists) {
+        knex.schema.createTable('users', function(table) {
+          table.increments('user_id').primary();
+          table.string('fb_name');
+          table.string('fb_id');
+        }).then(function (table) {
+          console.log("created users table")
+        })
+      }
+    }),
+
+    knex.schema.hasTable('restaurants').then(function (exists) {
+      if(!exists) {
+        exports.schema.createTable('restaurants', function(table) {
+          table.increments('rest_id').primary();
+          table.string('yelp_id');
+          table.string('name');
+          table.string('address');
+          table.string('phone');
+          table.string('price');
+          table.string('photo_url');
+        }).then(function (table) {
+          console.log("created restaurants table")
+        })
+      }
+    }),
+
+    knex.schema.hasTable('buckets').then(function (exists) {
+      if(!exists) {
+        knex.schema.createTable('buckets', function(table) {
+          table.increments();
+          table.foreign('user_id').references('users');
+          table.foreign('rest_id').references('restaurants');
+          table.string('category');
+        }).then(function (table) {
+          console.log("created buckets table")
+        })
+      }
+    })
+
+  ])
+}
 
