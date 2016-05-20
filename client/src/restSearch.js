3b69526ea0929805de8092232d94c685f5fbea4f
James Lee <jameslee.atx@gmail.com>
2016-05-20 16:10:43 -0500

    tile flex-wrap implemeneted - larger response file use

--- client/src/restSearch.js
+++ client/src/restSearch.js
@@ -2,48 +2,48 @@
 
 module.exports = function($scope, $http /*SeverService*/) {
 
-  //hardcode Franklin example
-  $scope.Franklin = {
-    name: 'Franklin Barbequeue',
-    rating: 4.5,
-    image: 'http://s3-media1.fl.yelpcdn.com/bphoto/hgki6r-7BTjxdeR7GshAIw/ms.jpg',
-    phone: '5124746300',
-    address: ["900 E 11th St",
-            "East Austin",
-            "Austin, TX 78702"],
-    categories: [['Barbecue', 'bbq']]
-  };
+  // //hardcode Franklin example
+  // $scope.Franklin = {
+  //   name: 'Franklin Barbequeue',
+  //   rating: 4.5,
+  //   image: 'http://s3-media1.fl.yelpcdn.com/bphoto/hgki6r-7BTjxdeR7GshAIw/ms.jpg',
+  //   phone: '5124746300',
+  //   address: ["900 E 11th St",
+  //           "East Austin",
+  //           "Austin, TX 78702"],
+  //   categories: [['Barbecue', 'bbq']]
+  // };
 
-  $scope.Branklin = {
-    name: 'Branklin Barbequeue',
-    rating: 4.5,
-    image: 'http://s3-media1.fl.yelpcdn.com/bphoto/hgki6r-7BTjxdeR7GshAIw/ms.jpg',
-    phone: '5124746300',
-    address: ["900 E 11th St",
-            "East Austin",
-            "Austin, TX 78702"],
-    categories: [['Barbecue', 'bbq']]
-  };
+  // $scope.Branklin = {
+  //   name: 'Branklin Barbequeue',
+  //   rating: 4.5,
+  //   image: 'http://s3-media1.fl.yelpcdn.com/bphoto/hgki6r-7BTjxdeR7GshAIw/ms.jpg',
+  //   phone: '5124746300',
+  //   address: ["900 E 11th St",
+  //           "East Austin",
+  //           "Austin, TX 78702"],
+  //   categories: [['Barbecue', 'bbq']]
+  // };
 
-  //hardcode Perry's example
-  $scope.Perrys = {
-    name: "Perry's Steakhouse & Grille",
-    rating: 4.0,
-    image: 'http://s3-media3.fl.yelpcdn.com/bphoto/-br-DGSTV4VcLroYw6kg1Q/ms.jpg',
-    phone: '5124746300',
-    address: ["114 W 7th St",
-            "Downtown",
-            "Austin, TX 78701"],
-    categories: [
-        ["Steakhouses",
-          "steak"
-        ],
-        [
-          'Seafood',
-          'seafood'
-        ]
-    ]
-  };
+  // //hardcode Perry's example
+  // $scope.Perrys = {
+  //   name: "Perry's Steakhouse & Grille",
+  //   rating: 4.0,
+  //   image: 'http://s3-media3.fl.yelpcdn.com/bphoto/-br-DGSTV4VcLroYw6kg1Q/ms.jpg',
+  //   phone: '5124746300',
+  //   address: ["114 W 7th St",
+  //           "Downtown",
+  //           "Austin, TX 78701"],
+  //   categories: [
+  //       ["Steakhouses",
+  //         "steak"
+  //       ],
+  //       [
+  //         'Seafood',
+  //         'seafood'
+  //       ]
+  //   ]
+  // };
 
   $scope.welcome = 'Search Module';
 
@@ -52,7 +52,7 @@ module.exports = function($scope, $http /*SeverService*/) {
     location: 'Austin'
   }
 
-  $scope.serverReply = [ $scope.Franklin, $scope.Perrys, $scope.Branklin ];
+  // $scope.serverReply = [ $scope.Franklin, $scope.Perrys, $scope.Branklin ];
 
   $scope.alertChange = function(){
     console.log($scope.searchInput.restName);
@@ -77,27 +77,111 @@ module.exports = function($scope, $http /*SeverService*/) {
   // }
 
 
+$scope.serverReply = [$scope.Franklin,$scope.Perrys,$scope.Uchi,$scope.Tacodeli,$scope.SalvationPizza,$scope.GussWorldFamousFriedChicken,$scope.KerbeyLaneCafe,$scope.MattsFamousElRancho];
+
+$scope.Franklin = {
+   name: 'Franklin Barbequeue',
+   rating: 4.5,
+   image: 'http://s3-media1.fl.yelpcdn.com/bphoto/hgki6r-7BTjxdeR7GshAIw/ms.jpg',
+   phone: '5124746300',
+   address: ["900 E 11th St",
+           "East Austin",
+           "Austin, TX 78702"],
+   categories: [['Barbecue', 'bbq']]
+ };
+
+
+$scope.Perrys = {
+   name: "Perry's Steakhouse & Grille",
+   rating: 4.0,
+   image: 'http://www.yelp.com/biz/perrys-steakhouse-and-grille-austin?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=CvAWWaBgIwh1nS0zjG_pxw',
+   phone: '5124746300',
+   address: ["114 W 7th St",
+                    "Ste 110",
+                    "Downtown",
+                    "Austin, TX 78701"],
+   categories: [['Steakhouses', 'Seafood']]
+ };
+
+
+$scope.Uchi = {
+   name: "Uchi",
+   rating: 4.5,
+   image: 'https://s3-media2.fl.yelpcdn.com/bphoto/yYzYBnO0bNjPx_9Az24lYQ/ms.jpg',
+   phone: '5129164808',
+   address: ["801 S Lamar Blvd",
+                    "South Lamar District",
+                    "Austin, TX 78704"],
+   categories: [['Sushi Bars', 'Japanese']]
+ };
+
+$scope.Tacodeli = {
+   name: "Tacodeli",
+   rating: 4.5,
+   image: 'http://s3-media3.fl.yelpcdn.com/bphoto/kPqO6RbiOlk6q5XNgU8IBA/ms.jpg',
+   phone: '5129164808',
+   address: ["1500 Spyglass Dr",
+                    "Ste B",
+                    "Barton Hills",
+                    "Austin, TX 78746"],
+   categories: [['Mexican', 'Vegetarian']]
+ };
+
+$scope.SalvationPizza = {
+   name: "Salvation Pizza",
+   rating: 3.5,
+   image: 'http://s3-media4.fl.yelpcdn.com/bphoto/QC9Jta0rsQ66XTdkIANIvg/ms.jpg',
+   phone: '5125350076',
+   address: ["624 W 34th St",
+                    "University of Texas",
+                    "Austin, TX 78705"],
+   categories: [['Pizza']]
+ };
+
+$scope.GussWorldFamousFriedChicken = {
+   name: "Gus's World Famous Fried Chicken",
+   rating: 4.0,
+   image: 'http://s3-media4.fl.yelpcdn.com/bphoto/veNBHKwcBbjuUzjp4iLubw/ms.jpg',
+   phone: '5124744877',
+   address: ["117 San Jacinto",
+                    "Downtown",
+                    "Austin, TX 78701"],
+   categories: [['Southern']]
+ };
+
+$scope.KerbeyLaneCafe = {
+   name: "Kerbey Lane Cafe",
+   rating: 4.0,
+   image: 'http://s3-media1.fl.yelpcdn.com/bphoto/SsogqJ4lb6cu1I_vNT_VFQ/ms.jpg',
+   phone: '5124511436',
+   address: ["3704 Kerbey Ln",
+                    "Austin, TX 78731"
+            ],
+   categories: [['Breakfast & Brunch']]
+ };
+
+ $scope.MattsFamousElRancho = {
+   name: "Matt's Famous El Rancho",
+   rating: 3.5,
+   image: 'https://s3-media2.fl.yelpcdn.com/bphoto/bkQAeaCQUmWUYaVCgIkt7Q/ms.jpg',
+   phone: '5124629333',
+   address: ["2613 S Lamar Blvd",
+                    "South Lamar District",
+                    "Austin, TX 78704"
+            ],
+   categories: [['Mexican']]
+ };
+
+
+$scope.MattsFamousElRancho = {
+   name: "Matt's Famous El Rancho",
+   rating: 3.5,
+   image: 'https://s3-media2.fl.yelpcdn.com/bphoto/bkQAeaCQUmWUYaVCgIkt7Q/ms.jpg',
+   phone: '5124629333',
+   address: ["2613 S Lamar Blvd",
+                    "South Lamar District",
+                    "Austin, TX 78704"
+            ],
+   categories: [['Mexican']]
+ };
 }
-
- // name: name,
-//       rating: rating,
-//       image: image_url,
-//       phone: phone,
-//       address: location
-//       category: category
-
-// angular.module('restaurantSearch', [])
-
-//   .controller('SearchCtrl', function($scope) {
-
-//   $scope.welcome = 'Search Moduel Working!';
-//   $scope.buttonText = 'Button working';
-
-//   var userInput = { name: "Franklin", location: "Austin" }
-
-//    // display selected list (wish/beenthere)
-//   $scope.searchByName = function ( str ) {
-
-//   }
- 
-// });
