c2f675b85d7458a7d0ca17351bd386946f613b96
MKS-students <mks.students@hackreactor.com>
2016-05-20 14:28:03 -0500

    Swapped to node-yelp, implemented API wrapper route

--- server/routes/yelp.js
+++ server/routes/yelp.js
@@ -1,25 +1,45 @@
-var yelp = require('node-yelp-api');
+var yelp = require('node-yelp');
 var merge = require('merge');
 var route = require('express').Router();
+var url   = require('url');
 
-var options = {
-  consumer_key: process.env.YELP_CONSUMER_KEY,
-  consumer_secret: process.env.YELP_CONSUMER_SECRET,
-  token: process.env.YELP_TOKEN,
-  token_secret: process.env.YELP_TOKEN_SECRET,
-};
+var client = yelp.createClient({
+  oauth: {
+    consumer_key: process.env.YELP_CONSUMER_KEY,
+    consumer_secret: process.env.YELP_CONSUMER_SECRET,
+    token: process.env.YELP_TOKEN,
+    token_secret: process.env.YELP_TOKEN_SECRET
+  }
+
+  // httpClients: {
+  //   maxSockets: 10
+  // }
+})
 
 route.get('/', function(req, res) {
-  yelp.search(merge(options, {
-    term: req.body.term,
-    location: req.body.location,
-    category_filter: 'restaurants'
-  }), function(data) {
-    res.status(200);
-    res.json(data);
-  }, function(err) {
-    res.status(500).send('Yelp API error');
-    console.log(data);
+  var queryData = url.parse(req.url, true).query;
+
+  console.log(queryData);
+
+  client.search({
+    term: queryData.term,
+    //category_filter: 'restaurants',
+    limit: 10,
+    location: queryData.location,
+    sort: 0
+  }).then(function(data) {
+    var modifiedData = data.businesses.map( (dat) => { return {
+      name: dat.name,
+
+      phone: dat.phone.slice(0, 3) + '-' + dat.phone.slice(3, 6) + '-' + dat.phone.slice(6, 10),
+      image: dat.image_url,
+      address: dat.location.display_address
+    }; })
+
+    res.status(200).json(modifiedData);
+  }).catch(function(err) {
+    console.log(err);
+    res.sendStatus(500);
   })
 });
 
