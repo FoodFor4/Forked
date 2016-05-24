588e237ed9199fc6804b0d0f0d4b48073bf5f2e3
Zak <legobug2@hotmail.com>
2016-05-24 10:39:40 -0500

    Added dependencies for Postgres, fixed part of Facebook login

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -5,13 +5,13 @@ var Users = require('../models/users');
 
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
-  clientSecrect: process.env.FACEBOOK_APP_SECRET,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
   callbackURL: "http://localhost:4000/auth/facebook/authed"
 },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
-    Users.findOrCreate({ facebookId: profile.id, facebookName: profile.name }, function (err, user) {
-      return cb(err, user);
+    Users.findOrCreate({ facebookId: profile.id, facebookName: profile.profileName }).then( function(data) {
+      console.log(data);
     });
   }
 ));
@@ -21,7 +21,7 @@ route.get('/facebook',
   passport.authenticate('facebook', { scope: ['user_friends', 'manage_pages']}));
 
 route.get('/facebook/authed',
-  passport.authenticate('facebook', {failureRedirect: '/login'}), function(req, res) {
+  passport.authenticate('facebook', {failureRedirect: '/#/login'}), function(req, res) {
     res.redirect('/');
   })
 
