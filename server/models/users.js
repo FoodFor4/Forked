03941da9b5ef5e82dcc4fb660aeb7e9c69a9d05a
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-05-24 11:39:02 -0500

    added User.create function

--- server/models/users.js
+++ server/models/users.js
@@ -4,6 +4,20 @@ var db = require('../db');
 var User = module.exports
 
 User.findOrCreate = function (incomingFbData) {
+// incomingFbData = { id: '1013808918710063',
+//  username: undefined,
+//  displayName: 'Zackery Perryman',
+//  name: 
+//   { familyName: undefined,
+//     givenName: undefined,
+//     middleName: undefined },
+//  gender: undefined,
+//  profileUrl: undefined,
+//  provider: 'facebook',
+//  _raw: '{"name":"Zackery Perryman","id":"1013808918710063"}',
+//  _json: { name: 'Zackery Perryman', id: '1013808918710063' } }
+
+//User.create = function (incomingFbData) {
 
   var attribs = Object.assign({}, incomingFbData)
 
@@ -13,7 +27,12 @@ User.findOrCreate = function (incomingFbData) {
     fb_id: attribs.facebookId,
     fb_name: attribs.facebookName
   };
+  // var userInfo = {};
 
+  // userInfo.fb_id = attribs.id;
+  // userInfo.fb_name = attribs.displayName; 
+  
+  console.log("created user: ", userInfo)
 
   return db('users').insert(userInfo).then(function(data) {
   	userInfo.id = data[0];
