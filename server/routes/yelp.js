bdea57ab9f22e8b1a4a63ec607ad754c41e6e644
Zak <legobug2@hotmail.com>
2016-05-25 15:52:21 -0500

    Phone hotfix

--- server/routes/yelp.js
+++ server/routes/yelp.js
@@ -3,6 +3,13 @@ var merge = require('merge');
 var route = require('express').Router();
 var url   = require('url');
 
+console.log({
+    consumer_key: process.env.YELP_CONSUMER_KEY,
+    consumer_secret: process.env.YELP_CONSUMER_SECRET,
+    token: process.env.YELP_TOKEN,
+    token_secret: process.env.YELP_TOKEN_SECRET
+  })
+
 var client = yelp.createClient({
   oauth: {
     consumer_key: process.env.YELP_CONSUMER_KEY,
@@ -30,7 +37,7 @@ route.get('/', function(req, res) {
       yelp_id: dat.id,
       name: dat.name,
       address: dat.location.display_address,
-      phone: dat.phone.slice(0, 3) + '-' + dat.phone.slice(3, 6) + '-' + dat.phone.slice(6, 10),
+      phone: dat.phone ? dat.phone.slice(0, 3) + '-' + dat.phone.slice(3, 6) + '-' + dat.phone.slice(6, 10) : 'No phone',
       categories: dat.categories.map( (arr) => arr[0] ),
       image: dat.image_url,
       yelp_rating: dat.rating
